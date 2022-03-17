8b4e195d67707974689ffdff45109642103d6cd3
Ernesto Freyre <ernestofreyreg@gmail.com>
2022-03-17 00:40:21 -0400

    added chakra

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
+import { Button } from '@chakra-ui/react'
 
 const Index = () => {
   return (
-    <div>Mint NFT</div>
+    <div>
+      <Button colorScheme='blue'>Button</Button>
+    </div>
   )
 }
 
 export default Index
-
